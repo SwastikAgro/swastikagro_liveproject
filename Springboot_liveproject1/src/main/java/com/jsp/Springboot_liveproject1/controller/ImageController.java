96fb657e20f2d8e94a020ad7727dd423a649da90
SwastikAgro86 <161795701+SwastikAgro@users.noreply.github.com>
2024-03-05 10:10:56 +0530

    Update ImageController.java

--- Springboot_liveproject1/src/main/java/com/jsp/Springboot_liveproject1/controller/ImageController.java
+++ Springboot_liveproject1/src/main/java/com/jsp/Springboot_liveproject1/controller/ImageController.java
@@ -3,12 +3,15 @@ package com.jsp.Springboot_liveproject1.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.jsp.Springboot_liveproject1.entity.Image;
 import com.jsp.Springboot_liveproject1.service.ImageService;
+import com.jsp.Springboot_liveproject1.util.ResponseStructure;
 
 
 @RestController
@@ -19,6 +22,9 @@ public class ImageController {
 	@PostMapping("/image")
 	public String  upload(@RequestParam MultipartFile file,@RequestParam String description) throws IOException {
 		return service.upload(file,description);
+		
+	}
+	public ResponseEntity<ResponseStructure<Image>> fetchImage(@RequestParam int id){
 		
 	}
 }
